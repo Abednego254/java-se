b16cb808acf22382c175a1fdb31dde389c987a08
Abednego254 <kaumeabednego3813@gmail.com>
2026-05-14 05:20:20 +0300

    More practice

---
+++ src/ExamTopics/dataTypes/Test.java
@@ -0,0 +1,10 @@
+package ExamTopics.dataTypes;
+
+public class Test {
+    public static void main(String[] args) {
+        short s1 = 200;
+        Integer s2 = 400;
+//        Long s3 = (short) s1 + s2;
+//        String s4 = (String) (s1 * s2)
+    }
+}
---
+++ src/modifiers/A.java
@@ -0,0 +1,14 @@
+package modifiers;
+
+public class A {
+    public void a() {
+        int a = 1;
+    }
+}
+
+class B {
+    private int doStuff() {
+        int x = 100;
+        return x++;
+    }
+}
