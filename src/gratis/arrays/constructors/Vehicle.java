b77087ce87c45e43a6d66677c111a8f7239496f8
Abednego254 <kaumeabednego3813@gmail.com>
2026-06-07 05:36:39 +0300

    Still practicing

---
+++ src/gratis/arrays/constructors/Vehicle.java
@@ -0,0 +1,27 @@
+//package gratis.arrays.constructors;
+//
+//public class Vehicle {
+//    int x;
+//    Vehicle() {
+//        this(10);
+//    }
+//
+//    Vehicle(int x) {
+//        this.x = x;
+//    }
+//}
+//
+//class Car extends Vehicle {
+//    int y;
+//    Car() {
+//        super();
+//        this(20);
+//    }
+//    Car(int y) {
+//        this.y = y;
+//    }
+//
+//    public String toString() {
+//        return super.x +" : "+ this.y;
+//    }
+//}
